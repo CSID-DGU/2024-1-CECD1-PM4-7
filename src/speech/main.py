0e0c91503f8258a36ca928ceb1bf846679dbee88
Kimdoodle <doodle@dgu.ac.kr>
2024-06-05 19:48:46 +0900

    Feat: STT training data creation

--- src/speech/main.py
+++ src/speech/main.py
@@ -11,9 +11,11 @@ auth.googleSTTAuth()
             = False: 파일을 선택하여 처리
     sliceWord = True: 변환된 텍스트를 단어들로 분리
             = False: 분리하지 않음
-    
+    makeTrainData = True: 변환 결과를 학습 데이터로도 생성
+            = False: 학습 데이터를 생성하지 않음
 '''
 STT.STT_pipeline(
-    askFolder=True,
-    sliceWord=False
+    askFolder=False,
+    sliceWord=False,
+    makeTrainData=True
 )
--- src/tuning/convert.py
+++ src/tuning/convert.py
@@ -2,6 +2,7 @@
 import json
 import pandas as pd
 import os
+from src.auth import getPrompt
 
 # 이미 완성된 xlsx인 경우 사용
 def complete_xlsx_to_jsonl(filepath: str):
@@ -28,34 +29,39 @@ def complete_xlsx_to_jsonl(filepath: str):
 
 
 # STT 결과 파일인 경우 사용
-def stt_xlsx_to_jsonl(filepath: str):
+def stt_xlsx_to_jsonl(sttResult: list, excelPath: str):
+    # Assistant content
+    df = pd.read_excel(excelPath)
+    df2 = df[['User content']]
+
+    if len(sttResult) != len(df2):
+        print("User content와 Assistant content의 길이가 다릅니다.")
+        print("데이터 확인이 필요합니다.")
+
     # 프롬프트
-    promptPath = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'public', 'prompt.json')
-    promptPath = os.path.abspath(promptPath)
-    with open(promptPath, 'r', encoding='utf-8') as f:
-        prompt_data = json.load(f)
-    prompt = prompt_data["stt_correction"]
-
-    # user input
-    df_user_input = pd.read_csv(filepath, usecols=[0])
-    user_input = df_user_input.iloc[:, 0]
-
-    # Todo: Assistant content
+    prompt = getPrompt("stt_correction")
 
     # JSONL 데이터 생성
     json_list = []
-    for user_text in user_input:
+    for i in range(len(df2)):
+        if i == 0:
+            continue
+        ac = df2.iloc[i]["User content"]
+        try:
+            uc = sttResult[i-1]
+        except IndexError:
+            uc = ""
         message = {
             "messages": [
                 {"role": "system", "content": prompt},
-                {"role": "user", "content": user_text},
-                {"role": "assistant", "content": ""}
+                {"role": "user", "content": uc},
+                {"role": "assistant", "content": ac}
             ]
         }
         json_list.append(json.dumps(message, ensure_ascii=False))
 
     # JSONL 파일 저장
-    output_file_path = os.path.splitext(filepath)[0] + '.jsonl'
+    output_file_path = os.path.splitext(excelPath)[0] + '_STT.jsonl'
     with open(output_file_path, 'w', encoding='utf-8-sig') as f:
         for item in json_list:
             f.write(item + '\n')
