446f856b9b63eb68bd42a38034cb995ab957baa9
KimDoodle <doodle@dgu.ac.kr>
2024-08-14 16:29:34 +0900

    Feat: STT correction model test code STT 교정모델을 테스트 할 수 있는 코드

--- src/tuning_STT/test.py
+++ src/tuning_STT/test.py
@@ -1,8 +1,44 @@
+# 모델 테스트 코드
 from openai import OpenAI
 from common.info import getPrompt, getModelName, open_dialog
 from common.auth_ import getKey
 from playground.client import send_request
 import pandas as pd
+
+def stt_correction_test():
+    PROMPT = getPrompt("stt_correction")
+    MODEL = getModelName("stt_correction")
+    KEY = getKey('STT')
+    client = OpenAI(api_key=KEY)
+
+    # DataFrame 초기화
+    filepath = open_dialog(False)
+    df = pd.read_excel(filepath)
+    result = []
+    for index, row in df.iterrows():
+        user_input = f"질문: {row['Question']}, 답변: {row['STT Result']}"
+
+        conversation_history = [
+            {
+                "role": "system",
+                "content": PROMPT
+            },
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ]
+
+        # OpenAI API 요청 및 응답 받기
+        assistant_response = send_request(client, conversation_history, user_input, MODEL)
+
+        # 응답 출력
+        print("Assistant:", assistant_response)
+        result.append(assistant_response)
+
+    df["Correction Result"] = result
+    df.to_excel(str(filepath).replace(".xlsx", "_corrected.xlsx"), index=False)
+
 
 def stt_validation_test():
     PROMPT = getPrompt("stt_validate")
@@ -40,3 +76,7 @@ def stt_validation_test():
     conversation_df["Validate Result"] = result
     conversation_df.to_excel(str(filepath).replace(".xlsx", "_validated.xlsx"), index=False)
     conversation_df.to_csv(str(filepath).replace(".xlsx", "_validated.csv"), index=False)
+
+
+if __name__ == '__main__':
+    stt_correction_test()
