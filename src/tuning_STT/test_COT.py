9a08a1c1acb7436467765c9f9c25ee693329ec8d
KimDoodle <doodle@dgu.ac.kr>
2024-09-02 18:44:48 +0900

    Feat: LLaMA training data creation - using GPT 2 COT 1단계만을 수행하는 코드 추가

--- src/tuning_STT/test_COT.py
+++ src/tuning_STT/test_COT.py
@@ -2,13 +2,53 @@
 import json
 
 import pandas as pd
+from openai import OpenAI
 
-from common.info import getPrompt, open_dialog
+from common.auth_ import getKey
+from common.info import getModelName, open_dialog
+from playground.client import send_request
+
+
+# GPT Playground를 사용하여 학습 데이터 생성
+def createData_fromGPT():
+    PROMPT = '"질문"에 의거하여 "답변"을 키워드 위주로 분석하라. 다음은 그 예시이다.\n질문: 건강 문제가 있으신가요?, 답변: 만성질환으로 몸이 아파\n예시 출력: 답변자는 "건강 문제"에 관한 질문에 "만성질환"으로 인해 "몸"이 아프다고 답변하였다.'
+    MODEL = "gpt-4o-mini"
+    KEY = getKey('STT')
+    client = OpenAI(api_key=KEY)
+
+    filePath = open_dialog(False, "파일을 선택하세요")
+    df = pd.read_csv(filePath)
+    result = []
+    for index, row in df.iterrows():
+        user_input = f'질문: {row["Question"]}, 답변: {row["STT Result"]}'
+
+        # 대화 기록 초기화
+        conversation_history = [
+            {
+                "role": "system",
+                "content": PROMPT
+            },
+            {
+                "role": "user",
+                "content": user_input
+            }
+        ]
+
+        # OpenAI API 요청 및 응답 받기
+        assistant_response = send_request(client, conversation_history, user_input, MODEL)
+
+        # 응답 출력
+        print(f"\n{index+1}/{len(df)}")
+        print(assistant_response)
+        result.append(assistant_response)
+
+    df["Assistant Content 1"] = result
+    newFilePath = str(filePath).replace(".csv", "_corrected.csv")
+    df.to_csv(newFilePath, encoding='utf-8-sig', index=False)
 
 
 # COT 1단계: 문장의 의도파악
 def stt_correction_LLaMA_COT1():
-    PROMPT = getPrompt("stt_correction")
     # DataFrame
     filepath = open_dialog(False)
     df = pd.read_csv(filepath)
@@ -19,7 +59,7 @@ def stt_correction_LLaMA_COT1():
         output = (row["Assistant Content 1"])
         
         message = {
-            "instruction": '질문에 대한 답변을 분석하라.',
+            "instruction": '"질문"에 의거하여 "답변"을 키워드 위주로 분석하라.',
             "input": user_input,
             "output": output,
         }
@@ -33,4 +73,5 @@ def stt_correction_LLaMA_COT1():
             f.write(item + '\n')
 
 if __name__ == '__main__':
+    # createData_fromGPT()
     stt_correction_LLaMA_COT1()
