5b73592eab3b99cf15fa6cb3b36053eebfdea449
KimDoodle <doodle@dgu.ac.kr>
2024-10-29 11:58:05 +0900

    Docs: STT validate model update STT 평가모델 업데이트

--- src/speech/validation/makedata.py
+++ src/speech/validation/makedata.py
@@ -1,15 +1,16 @@
-# STT 검증모델용
-
-from common.info import getPrompt, open_dialog
+# STT 평가모델 학습데이터 생성
+from common.info import getPrompt, getModelName, open_dialog
 from common.auth_ import getKey
 from playground.client import send_request_without_history
 from openai import OpenAI
 import pandas as pd
+import numpy as np
 import json
 import re
 
 KEY = getKey('OPENAI')
 client = OpenAI(api_key=KEY)
+
 
 # prompt = getPrompt("stt_validation_241015")
 
@@ -45,11 +46,12 @@ def makeData_byCorrectionModel():
         user_input = f'User: {row["User content"]}, Correct: row["Corrected"]'
 
         response = send_request_without_history(client, prompt1, user_input, "chatgpt-4o-latest")
-        print(f"{index+1}: {response}")
+        print(f"{index + 1}: {response}")
         result.append(response)
 
     df["validated"] = result
     df.to_excel(filePath.with_stem(filePath.stem + "_validated"), index=False)
+
 
 # 1. 교정모델 기반 데이터로 모델 학습 데이터 생성
 def makeTrainData_byCorrectionModel():
@@ -81,8 +83,46 @@ def makeTrainData_byCorrectionModel():
         for item in json_list:
             f.write(item + '\n')
 
+
+# 2. 대화모델 학습용 데이터를 기반으로 생성
+def makeData_byChatModel():
+    # 성능이 별로라 분리만 하고 수기로 수정할 계획
+    filePath = open_dialog(isfolder=False, filetypes=[("Excel Files", "*.xlsx")])
+    df = pd.read_excel(filePath, header=None)
+    prompt = "다음 질문과 답변은 전체 대화의 일부에 속한다. 질문과 답변이 매끄럽게 이어진다면 '통과'를, 그렇지 않다면 '부족'을 출력하라.질문에 대한 단답형 답변은 통과로 간주한다."
+
+    question = []
+    answer = []
+    result = []
+
+    for index, row in df.iterrows():
+        column = 2
+        try:
+            while True:
+                if len(row[column]) != 0 and len(row[column + 1]) != 0:
+                    question.append(row[column])
+                    answer.append(row[column + 1])
+                    user_input = f'질문: {row[column]}, 답변: {row[column + 1]}'
+                    print(user_input)
+                    response = send_request_without_history(client, prompt, user_input, "gpt-4o-mini")
+                    print(response + "\n")
+                    result.append(response)
+                    column += 2
+                else:
+                    break
+        except Exception as e:
+            continue
+
+    df2 = pd.DataFrame(zip(question, answer, result), columns=["Question", "Answer", "Assistant"])
+    df2.to_excel(filePath.with_stem(filePath.stem + "_trainData"), index=False)
+
+
+# 2. 대화모델 기반 데이터로 모델 학습 데이터 생성
+def makeTrainData_byChatModel():
+    pass
+
+
 if __name__ == '__main__':
     # makeData_byCorrectionModel()
-    makeTrainData_byCorrectionModel()
-
-
+    # makeTrainData_byCorrectionModel()
+    makeData_byChatModel()
